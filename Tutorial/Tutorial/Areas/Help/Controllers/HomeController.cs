db43834c15926e71ddf1e4b6fc2ab89927d4bfa5
Calvin Pierce <calvinpierce99@gmail.com>
2021-03-15 07:48:30 -0400

    Updated lab finished

---
+++ Tutorial/Tutorial/Areas/Help/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorial.Areas.Help.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            var contacts = new Dictionary<string, string>
+            {
+                { "Phone", "416-567-7890" },
+                { "Email", "JohnDoe@gmail.com" },
+                { "Facebook", "facebook.com/JohnDoe" }
+            };
+            return View(contacts);
+        }
+    }
+}
